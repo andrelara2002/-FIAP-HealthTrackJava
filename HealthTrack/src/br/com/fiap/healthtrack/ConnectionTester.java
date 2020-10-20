8c2be88a1211f2d7f51d34bba7076fe9ee3a7961
André Lara <66387919+andrelara2002@users.noreply.github.com>
2020-10-20 20:44:29 -0300

    Implementando Classe de testes para conexões funcional

--- HealthTrack/src/br/com/fiap/healthtrack/ConnectionTester.java
+++ HealthTrack/src/br/com/fiap/healthtrack/ConnectionTester.java
@@ -1,32 +1,56 @@
 package br.com.fiap.healthtrack;
-  
-  import java.sql.Connection;
-  import java.sql.DriverManager;
-  import java.sql.SQLException;
-  
-  public class ConnectionTester {
-  
-    public static void main(String[] args) {
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConnectionTester {
+
+  public static void main(String[] args) {
+    try {
+      // Registra o Driver
+      Class.forName("oracle.jdbc.driver.OracleDriver");
+
+      // Abre uma conexï¿½o
+      Connection conexao = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "RM85856",
+          "191102");
+
+      System.out.println("Conectado!");
+
       try {
-        //Registra o Driver
-        Class.forName("oracle.jdbc.driver.OracleDriver");
-  
-        //Abre uma conexão
-        Connection conexao = DriverManager.getConnection(
-            "jdbc:oracle:thin:@192.168.60.15:1521:ORCL", "OPS$PF0392", "123456");
+
+        java.sql.Statement stmt = conexao.createStatement();
+
+        PreparedStatement pstmt = conexao.prepareStatement("INSERT INTO T_USUARIO (cd_usuario, nm_usuario, nr_idadeusuario) VALUES (SEQ_USUARIO.NEXTVAL, ?, ?)");
+        pstmt.setString(1, "Rogerio");
+        pstmt.setInt(2, 12);
         
-        System.out.println("Conectado!");
-        
-        //Fecha a conexão
-        conexao.close();
-        
-      //Tratamento de erro  
-      } catch (SQLException e) {
-        System.err.println("Não foi possível conectar no Banco de Dados");
-        e.printStackTrace();
-      } catch (ClassNotFoundException e) {
-        System.err.println("O Driver JDBC não foi encontrado!");
+        pstmt.executeUpdate();
+
+        ResultSet result = stmt.executeQuery("SELECT * FROM T_USUARIO ORDER BY cd_usuario ASC");
+
+        while (result.next()){
+          System.out.println(result.getInt("cd_usuario")+" " + result.getString("nm_usuario")+" "+ result.getInt("nr_idadeusuario")+"\n");
+        }
+      }
+
+      catch (Exception e) {
         e.printStackTrace();
       }
+      // Fecha a conexï¿½o
+      conexao.close();
+
+      // Tratamento de erro
+    } catch (SQLException e) {
+      System.err.println("NÃ£o foi possÃ­vel conectar no Banco de Dados");
+      e.printStackTrace();
+
+    } catch (ClassNotFoundException e) {
+      System.err.println("O Driver JDBC nÃ£o foi encontrado!");
+      e.printStackTrace();
     }
   }
+}
