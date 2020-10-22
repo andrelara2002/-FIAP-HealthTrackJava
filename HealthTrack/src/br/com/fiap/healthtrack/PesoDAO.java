db709a097d39e028bf46061d152c0d15ee294dd9
André Lara <66387919+andrelara2002@users.noreply.github.com>
2020-10-22 17:22:09 -0300

    Introduzindo funções peso DAO

--- HealthTrack/src/br/com/fiap/healthtrack/PesoDAO.java
+++ HealthTrack/src/br/com/fiap/healthtrack/PesoDAO.java
@@ -1,6 +1,10 @@
 package br.com.fiap.healthtrack;
 
 import java.sql.SQLException;
+import java.util.Calendar;
+
+import oracle.net.aso.c;
+
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
@@ -24,10 +28,37 @@ public class PesoDAO {
                 stmt.setInt(4, cod_usuario);
                 
             } catch (Exception e) {
-                //TODO: handle exception
+                e.printStackTrace();
             }
         } catch (Exception e) {
-            //TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+    public void alterarDados(Peso peso, int cod_peso, int nr_peso, int nr_imc, int cod_usuario){
+        PreparedStatement stmt = null;
+        try {
+            conexao = ConexaoBanco.obterConexao();
+            try {
+                String sql = "UPDATE T_PESO SET nr_peso = ?, dt_peso = ?, nr_imc = ?, WHERE cd_usuario = ? AND cd_peso = ?";
+                stmt = conexao.prepareStatement(sql);
+                stmt.setInt(1, nr_peso);
+
+                Calendar c = Calendar.getInstance();
+                peso.setData(c);
+                java.sql.Date data = new java.sql.Date(peso.getData().getTimeInMillis());
+                stmt.setDate(2, data);
+
+                stmt.setInt(3, peso.getImc());
+                stmt.setInt(4, cod_usuario);
+                stmt.setInt(5, cod_peso);
+
+                stmt.executeUpdate();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
